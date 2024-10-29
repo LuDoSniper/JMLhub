30395daed270009fcb2ff83a08621428ee1a3cd2
Shivasjoke <jean-marc.thomain@hesias.fr>
2024-10-29 20:24:40 +0100

    Ajout base html + form + gestion auth

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\LoginFormType;
+use http\Env\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response{
+
+        $form = $this->createForm(LoginFormType::class);
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('Pages/login.html.twig', [
+            'form' => $form->createView(),
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void {}
+}
---
+++ src/Form/LoginFormType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use PhpParser\Node\Stmt\Label;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('_username', EmailType::class, [
+                'label' => 'Login',
+                'placeholder' => 'Email or Username',
+                'required' => true,
+            ])
+            ->add('_password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'placeholder' => 'Mot de passe',
+                'required' => true,
+            ])
+            ->add('_submit', SubmitType::class, [
+                'label' => 'Connexion'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
