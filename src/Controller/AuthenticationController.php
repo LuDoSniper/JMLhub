70619596d6834a9ba21688ad1b2c0b9ebf8b908b
LuDoSniper <lucien.donnarieix@hesias.fr>
2024-11-08 10:22:37 +0100

    Suppression de use inutiles

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -2,10 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
