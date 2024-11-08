548f73b15d85a62cb53785416a8bf20a67a1f93e
LuDoSniper <lucien.donnarieix@hesias.fr>
2024-11-08 12:46:21 +0100

    Feat[DEV-12]: Initialisation de la tâche

--- src/Command/UserCreateCommand.php
+++ src/Command/UserCreateCommand.php
@@ -2,12 +2,14 @@
 
 namespace App\Command;
 
-use App\Entity\User;
+use App\Entity\Authentication\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[AsCommand(
@@ -24,18 +26,38 @@ class UserCreateCommand extends Command
         parent::__construct();
     }
 
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('username', InputArgument::REQUIRED, 'The username of the new user')
+            ->addArgument('password', InputArgument::REQUIRED, 'The password of the new user')
+        ;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+
         $user = new User();
-        $user->setUsername('jm');
-        $user->setEmail('jm@jm.fr');
-        $user->setPlainpassword('jpp');
+        $username = $input->getArgument('username');
+        $password = $input->getArgument('password');
+
+        if (empty($username) || empty($password)) {
+            $io->error('Invalid arguments');
+
+            return Command::INVALID;
+        }
+
+        $user->setUsername($username);
+        $user->setPlainpassword($password);
 
         $hashedPassword = $this->hasher->hashPassword($user, $user->getPlainpassword());
         $user->setPassword($hashedPassword);
 
         $this->entityManager->persist($user);
         $this->entityManager->flush();
+
+        $io->success('User created');
 
         return Command::SUCCESS;
     }
