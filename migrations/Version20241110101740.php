16a71c5b9b3d1ca5723f75a4d0d2bbc4f056cff5
Mathys DC <rokassd9@gmail.com>
2024-11-10 11:29:09 +0100

    feat[FRT-06]: Default button style

---
+++ migrations/Version20241110101740.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241110101740 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE playlist RENAME INDEX fk_d782112da76ed395 TO IDX_D782112DA76ED395');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE playlist RENAME INDEX idx_d782112da76ed395 TO FK_D782112DA76ED395');
+    }
+}
