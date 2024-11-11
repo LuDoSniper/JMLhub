af3ffc9851b6df07961fdb88aa4481b1835c1e03
Mathys DC <rokassd9@gmail.com>
2024-11-11 16:49:29 +0100

    feat[DEV-33]: HomePage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie\Category;
+use App\Entity\Movie\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,10 +18,19 @@ class HomeController extends AbstractController
     }
 
     #[Route('/home', name: 'app_home')]
-    public function home(): Response
+    public function home(EntityManagerInterface $entityManager): Response
     {
+        // Récupérer le dernier film ajouté
+        $lastMovie = $entityManager->getRepository(Movie::class)->findOneBy([], ['id' => 'DESC']);
+
+        // Récupérer toutes les catégories avec leurs films associés
+        $categories = $entityManager->getRepository(Category::class)->findAll();
+
         return $this->render('Page/home.html.twig', [
-            'username'=> $this->getUser()->getUsername()
+            'username'=> $this->getUser()->getUsername(),
+            'lastMovie' => $lastMovie,
+            'categories' => $categories,
         ]);
     }
+
 }
