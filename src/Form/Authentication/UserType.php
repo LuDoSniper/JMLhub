1509c6ce42b2d8aa27627c79b78c82b734647475
Mathys DC <rokassd9@gmail.com>
2024-11-11 13:16:21 +0100

    feat[DEV-07]: UserType.php

---
+++ src/Form/Authentication/UserType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form\Authentication;
+
+use App\Entity\Authentication\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Username',
+                'attr' => [
+                    'placeholder' => 'Nom d\'utilisateur',
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'Email',
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Password',
+                    'attr' => ['placeholder' => 'Mot de passe'],
+                ],
+                'second_options' => [
+                    'label' => 'Confirm Password',
+                    'attr' => ['placeholder' => 'Confirmez le mot de passe'],
+                ],
+                'mapped' => false,
+                'required' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
