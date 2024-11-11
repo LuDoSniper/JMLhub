55a7dab4f1a0ffaf06aa2cbdc0642aab95d9bd08
Mathys DC <rokassd9@gmail.com>
2024-11-11 13:17:52 +0100

    feat[DEV-21]: ProfileType

---
+++ src/Form/Authentication/ProfileType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form\Authentication;
+
+use App\Entity\Authentication\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Username',
+                'attr' => [
+                    'placeholder' => 'Nom d\'utilisateur',
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'Email',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
