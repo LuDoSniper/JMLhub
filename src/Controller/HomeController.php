3d029e82e0fc78aff06ec05c79ae4f78053df8c1
LuDoSniper <lucien.donnarieix@hesias.fr>
2024-11-12 04:37:10 +0100

    Feat[FRT-34]: Création du bouton d'ajout aux playlists

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Movie\Category;
 use App\Entity\Movie\Movie;
+use App\Entity\Movie\Playlist;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +12,10 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        public EntityManagerInterface $entityManager
+    ){}
+
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
@@ -18,18 +23,22 @@ class HomeController extends AbstractController
     }
 
     #[Route('/home', name: 'app_home')]
-    public function home(EntityManagerInterface $entityManager): Response
+    public function home(): Response
     {
         // Récupérer le dernier film ajouté
-        $lastMovie = $entityManager->getRepository(Movie::class)->findOneBy([], ['id' => 'DESC']);
+        $lastMovie = $this->entityManager->getRepository(Movie::class)->findOneBy([], ['id' => 'DESC']);
 
         // Récupérer toutes les catégories avec leurs films associés
-        $categories = $entityManager->getRepository(Category::class)->findAll();
+        $categories = $this->entityManager->getRepository(Category::class)->findAll();
+
+        // Récupérer les playlists du user
+        $playlists = $this->entityManager->getRepository(Playlist::class)->findBy(['user' => $this->getUser()]);
 
         return $this->render('Page/home.html.twig', [
             'username'=> $this->getUser()->getUsername(),
             'lastMovie' => $lastMovie,
             'categories' => $categories,
+            'playlists' => $playlists
         ]);
     }
 
